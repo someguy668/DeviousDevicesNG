e46ccd432f38aa8aeeda448626a912420a99f816
Jaymon Kendall <jbk2000@gmail.com>
2026-09-11 00:15:35 -0600

    trying stuff

--- src/Main.cpp
+++ src/Main.cpp
@@ -30,7 +30,6 @@ namespace {
     {
         auto logsFolder = SKSE::log::log_directory();
         if (!logsFolder) SKSE::stl::report_and_fail("SKSE log_directory not provided, logs disabled.");
-        auto pluginName = SKSE::PluginDeclaration::GetSingleton()->GetName();
         auto logFilePath = *logsFolder / "DeviousDevicesNG.log";
         std::shared_ptr<spdlog::logger> loggerPtr;
         if (IsDebuggerPresent()) 
